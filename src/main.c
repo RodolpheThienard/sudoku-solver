8309e043e6289e7490c91cdc24c833e4896d55ac
RodolpheThienard <thienard.rodolphe1@gmail.com>
2024-02-21 11:06:02 +0100

    Performance measurement Escaping also gnuplot in .gitignore

--- src/main.c
+++ src/main.c
@@ -7,10 +7,26 @@
 #include <math.h>
 #include <omp.h>
 #include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
+#include <unistd.h>
 
-int
-main (int argc, char **argv)
+volatile int counter = 0;
+volatile int counter_err = 0;
+volatile int shouldTerminate = 0;
+
+void
+output_function ()
+{
+
+  printf ("%d;%d\n", counter, counter_err);
+  shouldTerminate = 1;
+
+  return;
+}
+
+void
+no_bench (int argc, char *argv[])
 {
   omp_set_dynamic (false);
 
@@ -56,7 +72,7 @@ main (int argc, char **argv)
       else if (solved)
         {
           __atomic_fetch_or (quit_ptr, true, __ATOMIC_SEQ_CST);
-          // fputs ("\nsuccess with result:\n", stdout);
+          fputs ("\nsuccess with result:\n", stdout);
           print_grd (blocks, 'v');
           break;
           /* abort (); */
@@ -70,6 +86,62 @@ main (int argc, char **argv)
                    omp_get_wtime () - start);
         }
     }
+}
 
+void
+bench (int argc, char *argv[])
+{
+  omp_set_dynamic (false);
+
+  argc = 3;
+  wfc_args args = wfc_parse_args (argc, argv);
+  const wfc_blocks_ptr init = wfc_load (0, args.data_file);
+
+  int quit = 0;
+  uint64_t iterations = 0;
+  wfc_blocks_ptr blocks = NULL;
+  pthread_mutex_t seed_mtx = PTHREAD_MUTEX_INITIALIZER;
+
+  int *volatile const quit_ptr = &quit;
+  uint64_t *volatile const iterations_ptr = &iterations;
+
+  const uint64_t max_iterations = count_seeds (args.seeds);
+  const double start = omp_get_wtime ();
+
+  signal (SIGALRM, output_function);
+  alarm (300);
+  uint64_t next_seed = 0;
+  while (!shouldTerminate)
+    {
+      pthread_mutex_lock (&seed_mtx);
+      next_seed++;
+      const bool has_next_seed = try_next_seed (&args.seeds, &next_seed);
+      pthread_mutex_unlock (&seed_mtx);
+
+      wfc_clone_into (&blocks, next_seed, init);
+      const bool solved = args.solver (blocks);
+      __atomic_add_fetch (iterations_ptr, 1, __ATOMIC_SEQ_CST);
+
+      if (solved)
+        {
+          __atomic_fetch_or (quit_ptr, true, __ATOMIC_SEQ_CST);
+          // fputs ("\nsuccess with result:\n", stdout);
+          // print_grd (blocks, 'v');
+          counter++;
+          // break;
+          /* abort (); */
+        }
+      else
+        {
+          counter_err++;
+        }
+    }
+}
+
+int
+main (int argc, char **argv)
+{
+  // no_bench (argc, argv);
+  bench (argc, argv);
   return 0;
 }
