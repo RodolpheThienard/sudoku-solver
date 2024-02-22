552930f973b567ff14aa7648889e23d4835b45a6
zakaria.ejjed <zakaria.ejjed@ens.uvsq.fr>
2024-02-22 22:42:45 +0100

    ADD - omp parallel on seed in main
    Now omp parallelizing seed access in main, if flag -lcpu is used we set
    the number of threads to 1 to force sequential execution, -lomp call the
    same solver but use whatever threads given.

--- src/main.c
+++ src/main.c
@@ -19,7 +19,6 @@ main (int argc, char **argv)
 
   int quit = 0;
   uint64_t iterations = 0;
-  wfc_blocks_ptr blocks = NULL;
   pthread_mutex_t seed_mtx = PTHREAD_MUTEX_INITIALIZER;
 
   int *volatile const quit_ptr = &quit;
@@ -27,26 +26,49 @@ main (int argc, char **argv)
 
   const uint64_t max_iterations = count_seeds (args.seeds);
   const double start = omp_get_wtime ();
+  bool already_solved = false;
 
+#pragma omp parallel 
   while (!*quit_ptr)
     {
+        wfc_blocks_ptr blocks = NULL;
+        bool has_next_seed = true;
+        uint64_t next_seed = 0;
+#pragma omp critical
+      {
       pthread_mutex_lock (&seed_mtx);
-      uint64_t next_seed = 0;
-      const bool has_next_seed = try_next_seed (&args.seeds, &next_seed);
+      next_seed = 0;
+      has_next_seed = try_next_seed (&args.seeds, &next_seed);
       pthread_mutex_unlock (&seed_mtx);
 
+      /* printf("seed: %lu - thread: %d - has_next_seed: %d\n", next_seed, omp_get_thread_num(), has_next_seed); */
+      }
       if (!has_next_seed)
         {
-          __atomic_fetch_or (quit_ptr, true, __ATOMIC_SEQ_CST);
-          fprintf (stderr, "no more seed to try\n");
           break;
         }
 
+      /* printf("Thread %d cloning into blocks\n", omp_get_thread_num()); */
       wfc_clone_into (&blocks, next_seed, init);
+      /* printf("Thread %d attemping to solve using seed %lu\n", omp_get_thread_num(), next_seed); */
       const bool solved = args.solver (blocks);
+      /* printf("Thread %d ended solving using seed %lu\n", omp_get_thread_num(), next_seed); */
       __atomic_add_fetch (iterations_ptr, 1, __ATOMIC_SEQ_CST);
+      /* if (next_seed == 1 && !solved) */
+      /*   { */
+      /*       printf("Thread %d result on seed 1\n", omp_get_thread_num()); */
+      /*       print_grd(blocks, 'v'); */
+      /*   } */
 
-      if (solved && args.output_folder != NULL)
+
+#pragma omp critical
+     {
+      if (already_solved)
+        {
+            printf("\nThread %d stopped and rejoy from the result of another thread\n", omp_get_thread_num());
+          __atomic_fetch_or (quit_ptr, true, __ATOMIC_SEQ_CST);
+        }
+      else if (solved && args.output_folder != NULL)
         {
           __atomic_fetch_or (quit_ptr, true, __ATOMIC_SEQ_CST);
           fputc ('\n', stdout);
@@ -55,20 +77,27 @@ main (int argc, char **argv)
 
       else if (solved)
         {
+            printf("\nSolved by thread %d using seed %lu\n", omp_get_thread_num(), next_seed);
           __atomic_fetch_or (quit_ptr, true, __ATOMIC_SEQ_CST);
           // fputs ("\nsuccess with result:\n", stdout);
           print_grd (blocks, 'v');
-          break;
+          already_solved = true;
           /* abort (); */
         }
 
       else if (!*quit_ptr)
         {
-          fprintf (stdout, "\r%.2f%% -> %.2fs",
+          fprintf (stdout, "\r%.2f%% -> %.2fs - seed %lu",
                    ((double)(*iterations_ptr) / (double)(max_iterations))
                        * 100.0,
-                   omp_get_wtime () - start);
+                   omp_get_wtime () - start, next_seed);
         }
+    }
+
+     if (solved)
+       {
+         break;
+       }
     }
 
   return 0;
--- src/solvers/sudoku_omp.c
+++ src/solvers/sudoku_omp.c
@@ -10,36 +10,52 @@ solve_openmp (wfc_blocks_ptr blocks)
 {
   uint64_t iteration = 0;
   const uint64_t seed = blocks->seed;
+  struct
+  {
+    uint32_t gy, x, y, _1;
+    uint64_t state;
+  } row_changes[blocks->grid_side];
 
+  entropy_location min = { 0, 0 };
   entropy_location blk_entropy = { 0, 0 };
+  vec2 grd_location = { 0, 0 };
   bool error = false;
 
-  printf("Solver using OpenMP\n");
   forever
   {
     bool changed = false;
-#pragma omp parallel for collapse(2) shared(blocks, changed) private(blk_entropy) num_threads(blocks->grid_side*blocks->grid_side)
+    min.entropy = UINT8_MAX;
+    // 1. Collapse
     for (uint32_t gx = 0; gx < blocks->grid_side; gx++)
       {
         for (uint32_t gy = 0; gy < blocks->grid_side; gy++)
           {
             blk_entropy = blk_min_entropy (blocks, gx, gy);
-            if (blk_entropy.entropy > 1 && blk_entropy.entropy < UINT8_MAX)
+            if (blk_entropy.entropy < min.entropy)
               {
                 changed = true;
-                // 1. Collapse
-                uint64_t *blk_ptr = blk_at (blocks, gx, gy,
-                        blk_entropy.location.x, blk_entropy.location.y);
-                *blk_ptr = entropy_collapse_state (*blk_ptr, gx,
-                                                   gy, blk_entropy.location.x,
-                                                   blk_entropy.location.y, seed, iteration);
-                // 2. Propagate
-                uint64_t collapsed = *blk_ptr;
-                error = all_propagate (blocks, gx, gy, blk_entropy.location.x,
-                               blk_entropy.location.y, collapsed);
+                min.entropy = blk_entropy.entropy;
+                min.location.x = blk_entropy.location.x;
+                min.location.y = blk_entropy.location.y;
+                grd_location.x = gx;
+                grd_location.y = gy;
               }
           }
       }
+
+    uint64_t *blk_ptr = blk_at (blocks, grd_location.x, grd_location.y,
+                                min.location.x, min.location.y);
+    *blk_ptr = entropy_collapse_state (*blk_ptr, grd_location.x,
+                                       grd_location.y, min.location.x,
+                                       min.location.y, seed, iteration);
+
+    // 2. Propagate
+
+    uint64_t collapsed = *blk_ptr;
+    error = all_propagate (blocks, grd_location.x, grd_location.y, min.location.x,
+                   min.location.y, collapsed);
+
+    // 3. Check Error
 
     iteration += 1;
     if (!changed || error)
